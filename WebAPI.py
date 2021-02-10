4e5d128723652292ad8b8ca93e8d7a36756dfcb5
Muhammad Kashan <63954509+Kashan8876@users.noreply.github.com>
2021-02-10 15:12:50 +0500

    Add files via upload

---
+++ WebAPI.py
@@ -0,0 +1,49 @@
+from flask import Flask , jsonify
+
+app = Flask(__name__)
+
+FYP_Task = [{
+
+
+   "id" :1,
+   "title" : "Hello World",
+
+
+
+
+},
+{
+
+
+   "id" :2,
+   "title" : "Muhammad Kashan",
+
+
+
+
+},
+{
+
+
+   "id" :3,
+   "title" : "Smart Health Consultant",
+
+
+
+
+}
+
+
+]
+
+
+
+@app.route('/',methods = ["GET","POST"])
+def get_book():
+    return jsonify ({"FYP_Task" : FYP_Task})
+
+if __name__ == "__main__":
+    
+    app.run(port=5000)
+
+
