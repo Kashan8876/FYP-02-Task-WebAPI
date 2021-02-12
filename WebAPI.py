24453ee87241466ea5237ec77f29ddf880b7a8bc
Muhammad Kashan <63954509+Kashan8876@users.noreply.github.com>
2021-02-12 16:57:23 +0500

    Update WebAPI.py

--- WebAPI.py
+++ WebAPI.py
@@ -2,16 +2,29 @@ from flask import Flask , jsonify
 
 app = Flask(__name__)
 
+FYP_Task = [{
+
+    "id":1,
+    "title":"Smart Health Consultant",
+},
+{
+    
+    "id":2,
+    "title":"Hello World",
+},
+{
+    
+    "id":3,
+    "title":"Muhammad Kashan",
+
+}]
 
 @app.route('/', methods=['GET','POST'])
-def home():
-   
-   
-   return "<h1>Smart Health Consultant</h1><p>Hello World..!</p>"
+def get_fyp():
+
+    return jsonify(FYP_Task)
 if __name__ == "__main__":
-   
-   
     
-   app.run(threaded=True,port=5000)
+    app.run(threaded=True,port=5000)
 
 
